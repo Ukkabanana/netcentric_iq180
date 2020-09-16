7ef0725c9ee107f9cd1d43579f90afb25df85405
AlphaNiner1415 <manchesternd@gmail.com>
2020-09-16 22:36:27 +0700

    changed indent on index.js

--- index.js
+++ index.js
@@ -4,27 +4,27 @@ var http = require("http").createServer(app);
 var io = require("socket.io")(http); //Initialize new instance of socket.io by passing in the Http object
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", (socket) => {
   //Listening on connection for incoming sockets
-  console.log("a user connected");
-  socket.broadcast.emit("A user connected"); //Send message to everyone but the sender
+    console.log("a user connected");
+    socket.broadcast.emit("A user connected"); //Send message to everyone but the sender
 
-  socket.on("disconnect", () => {
+    socket.on("disconnect", () => {
     //On disconnect socket
-    console.log("user disconnected");
-    socket.broadcast.emit("A user disconnected");
-  });
+        console.log("user disconnected");
+        socket.broadcast.emit("A user disconnected");
+    });
   //   socket.on("chat message", (msg) => { //User emitting a message
   //     console.log("message: " + msg);
   //   });
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg); //Send to everyone including sender
+    socket.on("chat message", (msg) => {
+        io.emit("chat message", msg); //Send to everyone including sender
   });
 });
 
 http.listen(3000, () => {
-  console.log("listening on *:3000");
+    console.log("listening on *:3000");
 });
