e7c5c5be07678eec87e0777ee54c7377c98f85a0
AlphaNiner1415 <manchesternd@gmail.com>
2020-09-17 18:29:03 +0700

    added prettier formatter, and start randomnumber generator

--- index.js
+++ index.js
@@ -1,30 +1,47 @@
-var express = require("express");
+const { randomInt } = require('crypto');
+var express = require('express');
 var app = express();
-var http = require("http").createServer(app);
-var io = require("socket.io")(http); //Initialize new instance of socket.io by passing in the Http object
+var http = require('http').createServer(app);
+var io = require('socket.io')(http); //Initialize new instance of socket.io by passing in the Http object
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on("connection", (socket) => {
-  //Listening on connection for incoming sockets
-    console.log("a user connected");
-    socket.broadcast.emit("A user connected"); //Send message to everyone but the sender
+app.get('/genNewNumber', (req, res) => {
+  res.send(numberGenerator());
+    
+});
 
-    socket.on("disconnect", () => {
-    //On disconnect socket
-        console.log("user disconnected");
-        socket.broadcast.emit("A user disconnected");
+const numberGenerator = () => {
+    var array= ["+","*","-","/"];
+    var num1 = Math.round(Math.random()*10);
+    var num2 = Math.round(Math.random()*10);
+    var num3 = Math.round(Math.random()*10);
+    var num4 = Math.round(Math.random()*10);
+    var num5 = Math.round(Math.random()*10);
+    var choice1 = Array.random
+    return "" + num1 + " " + num2 + num3 + num4 + num5;
+}
+
+io.on('connection', (socket) => {
+    //Listening on connection for incoming sockets
+    console.log('a user connected');
+    socket.broadcast.emit('A user connected'); //Send message to everyone but the sender
+
+    socket.on('disconnect', () => {
+        //On disconnect socket
+        console.log('user disconnected');
+        socket.broadcast.emit('A user disconnected');
     });
-  //   socket.on("chat message", (msg) => { //User emitting a message
-  //     console.log("message: " + msg);
-  //   });
-    socket.on("chat message", (msg) => {
-        io.emit("chat message", msg); //Send to everyone including sender
-  });
+    //   socket.on("chat message", (msg) => { //User emitting a message
+    //     console.log("message: " + msg);
+    //   });
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg); //Send to everyone including sender
+    });
 });
 
 http.listen(3000, () => {
-    console.log("listening on *:3000");
+    console.log('listening on *:3000');
 });
